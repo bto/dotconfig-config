e298037ce0f494966b77f8e32e2a51dbe8a69c3a
Masato Bito <m@btmst.jp>
2020-08-11 14:48:28 +0900

    add ssh-agent

---
+++ modules/ssh-agent/config.fish
@@ -0,0 +1,3 @@
+if type -sq fish_ssh_agent
+    fish_ssh_agent
+end
---
+++ modules/ssh-agent/init.fish
@@ -0,0 +1,3 @@
+curl \
+  https://raw.githubusercontent.com/ivakyb/fish_ssh_agent/master/functions/fish_ssh_agent.fish \
+  --create-dirs -sLo ~/.config/fish/functions/fish_ssh_agent.fish
