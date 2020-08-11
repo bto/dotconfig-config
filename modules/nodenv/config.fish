e41c91f3dd3c569f23b056f110353173c17a9322
Masato Bito <m@btmst.jp>
2020-08-11 13:34:49 +0900

    add phpenv

--- modules/nodenv/config.fish
+++ modules/nodenv/config.fish
@@ -1,5 +1,6 @@
-dotconfig set_path PATH ~/.nodenv/bin
+set -gx NODENV_ROOT ~/.nodenv
+dotconfig set_path PATH $NODENV_ROOT/bin
 
-if type -sq nodenv
-    source (nodenv init - | psub)
+if type -sq nodenv; and status --is-interactive
+    source (nodenv init -|psub)
 end
---
+++ modules/phpenv/init.fish
@@ -0,0 +1,3 @@
+if not test -d ~/.phpenv
+    curl -L https://raw.githubusercontent.com/phpenv/phpenv-installer/master/bin/phpenv-installer | bash
+end
