4a23aa5403fc844e8aed691810d99fe3db472a9a
Masato Bito <m@btmst.jp>
2020-08-11 16:51:10 +0900

    add installer

---
+++ bin/installer.fish
@@ -0,0 +1,17 @@
+set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+set dotconfig_config_dir $XDG_CONFIG_HOME/dotconfig
+set fish_config_dir $XDG_CONFIG_HOME/fish
+set fish_function_dir $fish_config_dir/functions
+set dotconfig_file $fish_function_dir/dotconfig.fish
+
+mkdir -p $fish_function_dir
+
+if not test -f $dotconfig_file
+    curl \
+        https://raw.githubusercontent.com/bto/dotconfig/master/dotconfig.fish \
+        -sLo $dotconfig_file
+end
+
+if not test -d $dotconfig_config_dir
+    git clone https://github.com/bto/dotconfig-files $dotconfig_config_dir
+end
