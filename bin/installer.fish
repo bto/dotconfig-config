460291cce48536bdb86ccdfa5549cecba67904db
Masato Bito <m@btmst.jp>
2020-08-11 17:56:14 +0900

    initialize config.fish

--- bin/installer.fish
+++ bin/installer.fish
@@ -1,6 +1,7 @@
 set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 set dotconfig_config_dir $XDG_CONFIG_HOME/dotconfig
 set fish_config_dir $XDG_CONFIG_HOME/fish
+set fish_config_file $fish_config_dir/config.fish
 set fish_function_dir $fish_config_dir/functions
 set dotconfig_file $fish_function_dir/dotconfig.fish
 
@@ -14,4 +15,8 @@ end
 
 if not test -d $dotconfig_config_dir
     git clone https://github.com/bto/dotconfig-files $dotconfig_config_dir
+end
+
+if not test -f $fish_config_file; or not grep -q 'dotconfig *load' $fish_config_file
+    echo 'type -q dotconfig; and dotconfig load' >> $fish_config_file
 end
