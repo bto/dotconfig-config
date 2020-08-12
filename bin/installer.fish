450449c328f18115edeecef5db91ce0d856029e9
Masato Bito <m@btmst.jp>
2020-08-12 19:30:14 +0900

    fix

--- bin/installer.fish
+++ bin/installer.fish
@@ -7,7 +7,7 @@ fisher add bto/dotconfig
 # install dotconfig-config
 set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
 set config_dir $XDG_CONFIG_HOME/dotconfig
-if test -d $config_dir
+if not test -d $config_dir
     git clone https://github.com/bto/dotconfig-config $config_dir
 end
 
