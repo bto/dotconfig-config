bac929480b2e504a04fb5f9e35e1c232f33f95d8
Masato Bito <m@btmst.jp>
2020-08-12 19:57:43 +0900

    use setup subcommand

--- bin/installer.fish
+++ bin/installer.fish
@@ -4,12 +4,5 @@ curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fi
 # install dotconfig
 fisher add bto/dotconfig
 
-# install dotconfig-config
-set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-set config_dir $XDG_CONFIG_HOME/dotconfig
-if not test -d $config_dir
-    git clone https://github.com/bto/dotconfig-config $config_dir
-end
-
-# initialize
-dotconfig init
+# setup by dotconfig
+dotconfig setup bto/dotconfig-config
