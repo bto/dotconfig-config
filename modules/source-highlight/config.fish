bbdd5c211e73987e6d8823a9bfddb2f0aa4c399f
Masato Bito <m@btmst.jp>
2020-09-02 00:39:53 +0900

    fix source-highlight for ubuntu

--- modules/source-highlight/config.fish
+++ modules/source-highlight/config.fish
@@ -1,3 +1,8 @@
 if type -q src-hilite-lesspipe.sh
     set -gx LESSOPEN '| src-hilite-lesspipe.sh %s'
 end
+
+set shell_file /usr/share/source-highlight/src-hilite-lesspipe.sh
+if test -x $shell_file
+    set -gx LESSOPEN "| $shell_file %s"
+end
