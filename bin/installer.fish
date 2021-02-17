800401a9740ad0475b2d4ee00c9b2b28bdbbf433
Masato Bito <m@btmst.jp>
2021-02-17 13:55:39 +0900

    s/add/install/

--- bin/installer.fish
+++ bin/installer.fish
@@ -2,7 +2,7 @@
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
 
 # install dotconfig
-fisher add bto/dotconfig
+fisher install bto/dotconfig
 
 # setup by dotconfig
 dotconfig setup bto/dotconfig-config
