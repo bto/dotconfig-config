a88196b3ae27b43d9a4a1ee236a7f50148c7c079
Masato Bito <m@btmst.jp>
2022-07-09 23:27:50 +0900

    remove nodenv

--- modules/nodenv/config.fish
+++
@@ -1,6 +0,0 @@
-set -gx NODENV_ROOT ~/.nodenv
-dotconfig set_path PATH $NODENV_ROOT/bin
-
-if type -q nodenv; and status --is-interactive
-    source (nodenv init - | psub)
-end
--- modules/nodenv/init.fish
+++
@@ -1,3 +0,0 @@
-if not test -d ~/.nodenv
-    curl -fsSL https://raw.githubusercontent.com/nodenv/nodenv-installer/master/bin/nodenv-installer | bash
-end
