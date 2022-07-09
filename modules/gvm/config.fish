9f8167137f8cb24203045752f95626129bee3b6a
Masato Bito <m@btmst.jp>
2022-07-09 23:14:32 +0900

    remove gvm

--- modules/gvm/config.fish
+++
@@ -1,3 +0,0 @@
-if type -q bass; and test -d ~/.gvm
-    bass source ~/.gvm/scripts/gvm
-end
--- modules/gvm/init.fish
+++
@@ -1,3 +0,0 @@
-if not test -d ~/.gvm
-    curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
-end
