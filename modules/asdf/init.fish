d5e2c90f8f8b5bc730e40de8030274b34d4b5166
Masato Bito <m@btmst.jp>
2022-09-19 16:12:32 +0900

    fix a bit

--- modules/asdf/init.fish
+++ modules/asdf/init.fish
@@ -1,3 +1,3 @@
-if test -f ~/opt/asdf/completions/asdf.fish
+if test -f ~/opt/asdf/completions/asdf.fish -a ! -f ~/.config/fish/completions/asdf.fish
     ln -s ~/opt/asdf/completions/asdf.fish ~/.config/fish/completions
 end
